25a0c22e0a35f9c1ea9b57a105cf80db6381aad9
Will Hawkins <hawkinsw@opentechinstitute.org>
2014-05-19 00:09:38 -0400

    Add UI and storage support for tests.
    1. Storage: store the results when they
       are available.
    2. Report results to the UI when they are
       available.

--- data/sidebar.js
+++ data/sidebar.js
@@ -1,3 +1,12 @@
 function startTest(testName) {
   addon.port.emit("startTest", testName);
 }
+
+addon.port.on("testDone", function (test) {
+  var testTime, testTest;
+  
+  testTime = test.time;
+  testTest = test.test;
+
+  alert("test (" + testTest + ") was done at " + testTime);
+});
