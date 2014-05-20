9f1fdebba52941ff0da3adae7ed53721f22376ab
Will Hawkins <hawkinsw@opentechinstitute.org>
2014-05-20 19:33:53 -0400

    First crack at graphing results.

--- data/sidebar.js
+++ data/sidebar.js
@@ -1,14 +1,137 @@
+/* vim: set expandtab ts=2 sw=2: */
 function startTest(testName) {
   addon.port.emit("startTest", testName);
 }
 
 function listTestResults(testName) {
   addon.port.emit("listTestResults", testName);
+  getTestResults(testName);
 }
 
 function getTestResult(testName, testTime) {
   addon.port.emit("getTestResult", { test: testName, time: testTime});
 }
+
+function getTestResults(testName) {
+  addon.port.emit("getTestResults", testName);
+}
+
+addon.port.on("testResults", function (results) {
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = 260 - margin.left - margin.right,
+      height = 200 - margin.top - margin.bottom;
+
+  var x = d3.time.scale()
+      .range([0, width]);
+
+  var y = d3.scale.linear()
+      .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  var line = d3.svg.line()
+      .interpolate("basis")
+      .x(function(d) { return x(d.time); })
+      .y(function(d) { return y(d.value); });
+
+  var resultGraphArea = document.getElementById("GraphResultsArea");
+  while (resultGraphArea.firstChild) {
+    resultGraphArea.removeChild(resultGraphArea.firstChild);
+  }
+  var svg = d3.select("#GraphResultsArea").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var data = [
+  {
+    test: "C2S",
+    values: []
+  },
+  {
+    test: "S2C",
+    values: []
+  }];
+
+  function findTestIndex(testName, data) {
+    for (i in data) {
+      if (data[i].test == testName) return i;
+    }
+    return 0;
+  }
+
+  for (i in results.results) {
+    var resultsTime = new Date(results.results[i].time);
+    var resultsParsed = JSON.parse(results.results[i].results);
+    var newValue = {
+      time: resultsTime,
+      value: parseInt(resultsParsed["C2S"].throughput, 10)/1000.0
+      };
+    data[findTestIndex("C2S", data)].values.push(newValue);
+    newValue = {
+      time: resultsTime,
+      value: parseInt(resultsParsed["S2C"].throughput, 10)/1000.0
+      };
+    data[findTestIndex("S2C", data)].values.push(newValue);
+  }
+
+  x.domain([
+    d3.min(data, function(d) {
+      return d3.min(d.values, function(v) {
+        return v.time;
+      })
+    }),
+    d3.max(data, function(d) {
+      return d3.max(d.values, function(v) {
+        return v.time;
+      })
+    }),
+  ]);
+
+  y.domain([
+    d3.min(data, function(d) {
+      return d3.min(d.values, function(v) {
+        return v.value;
+      })
+    }),
+    d3.max(data, function(d) {
+      return d3.max(d.values, function(v) {
+        return v.value;
+      })
+    }),
+  ]);
+
+  svg.append("g")
+     .attr("class", "x axis")
+     .attr("transform", "translate(0," + height + ")")
+     .call(xAxis);
+
+  svg.append("g")
+     .attr("class", "y axis")
+     .call(yAxis)
+     .append("text")
+     .attr("transform", "rotate(-90)")
+     .attr("y", 6)
+     .attr("dy", ".71em")
+     .style("text-anchor", "end")
+     .text("Speed (Mbps)");
+
+  var testLine = svg.selectAll(".test")
+                .data(data)
+                .enter().append("g")
+                .attr("class", "testLine");
+
+  testLine.append("path")
+      .attr("class", "line")
+      .attr("d", function(d) { return line(d.values); });
+});
 
 addon.port.on("testResult", function (test) {
   var testTest, testTime, testResults;
@@ -25,7 +148,7 @@ addon.port.on("testResult", function (test) {
 
 addon.port.on("testDone", function (test) {
   var testTime, testTest;
-  
+
   testTime = test.time;
   testTest = test.test;
 
